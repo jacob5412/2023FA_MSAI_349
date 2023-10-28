f2dbf1e479bf4e58999f5992c000d7609e596832
Jacob <kapsjacob@gmail.com>
2023-10-28 12:21:12 -0500

    add docstrings

--- kmeans-knn/run_kmeans.py
+++ kmeans-knn/run_kmeans.py
@@ -12,6 +12,15 @@ from kmeans import KMeans
 
 
 def read_data(file_name):
+    """
+    Read data from a CSV file and return it as a list of label-feature pairs.
+
+    Args:
+        file_name (str): The name of the CSV file to read.
+
+    Returns:
+        list: A list of label-feature pairs.
+    """
     data_set = []
     with open(file_name, "rt") as f:
         for line in f:
@@ -26,11 +35,29 @@ def read_data(file_name):
 
 
 def get_numerical_labels(data_set):
+    """
+    Extract numerical labels from a list of label-feature pairs.
+
+    Args:
+        data_set (list): A list of label-feature pairs.
+
+    Returns:
+        list: A list of numerical labels.
+    """
     labels = [int(row[0]) for row in data_set]
     return labels
 
 
 def get_numerical_features(data_set):
+    """
+    Extract numerical features from a list of label-feature pairs.
+
+    Args:
+        data_set (list): A list of label-feature pairs.
+
+    Returns:
+        list: A list of numerical features.
+    """
     features = [[int(datapoint) for datapoint in row[1]] for row in data_set]
     return features
 
