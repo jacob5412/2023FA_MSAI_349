7774fbc735ade545327e3dcd595c325c5026a171
Jacob <kapsjacob@gmail.com>
2023-10-29 21:49:27 -0500

    Testing K-means & K-NNs on Iris

--- kmeans-knn/run_test_kmeans.py
+++ kmeans-knn/run_test_kmeans.py
@@ -17,20 +17,35 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("kmeans-training")
 
 if __name__ == "__main__":
-    validation_set = read_data("valid.csv")
-    validation_set_labels = np.array(get_numerical_labels(validation_set))
-    validation_set_features = np.array(get_numerical_features(validation_set))
+    training_set = read_data("iris_dataset/train.csv")
+    validation_set = read_data("iris_dataset/valid.csv")
+    validation_set_labels = np.array(get_numerical_labels(validation_set, int))
+    validation_set_features = np.array(get_numerical_features(validation_set, float))
+    training_set_labels = np.array(get_numerical_labels(training_set, int))
+    training_set_features = np.array(get_numerical_features(training_set, float))
 
-    n_clusters = 10
-    soft_kmeans = KMeans(n_clusters)
-    soft_kmeans.fit(validation_set_features, "euclidean")
-    predicted_labels = soft_kmeans.predict(
+
+    n_clusters = 3
+    kmeans = KMeans(n_clusters)
+    print("--- Training K-means on Iris ---\n")
+    kmeans.fit(training_set_features, "euclidean")
+    predicted_labels = kmeans.predict(
+        training_set_features, training_set_labels, "euclidean"
+    )
+    confusion_mat = create_confusion_matrix(
+        n_clusters, training_set_labels, predicted_labels
+    )
+    display_confusion_matrix(confusion_mat)
+    eval_metrics = eval_metrics_from_confusion_matrix(confusion_mat)
+    display_eval_metrics(eval_metrics)
+
+    print("--- Testing K-means on Iris ---\n")
+    predicted_labels = kmeans.predict(
         validation_set_features, validation_set_labels, "euclidean"
     )
     confusion_mat = create_confusion_matrix(
         n_clusters, validation_set_labels, predicted_labels
     )
     display_confusion_matrix(confusion_mat)
-
     eval_metrics = eval_metrics_from_confusion_matrix(confusion_mat)
     display_eval_metrics(eval_metrics)
